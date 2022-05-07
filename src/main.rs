418bd5c2822ba4ac59504be9aac516a06bf14f8f
LucDachary <luc.dachary@fastmail.com>
2022-05-07 20:59:17 +0200

    Adds base for advertising.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use bluer::adv::Advertisement;
+use std::time::Duration;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    time::sleep,
+};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> bluer::Result<()> {
+    env_logger::init();
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+    adapter.set_powered(true).await?;
+
+    println!(
+        "Advertising on Bluetooth adapter {} with address {}",
+        adapter.name(),
+        adapter.address().await?
+    );
+    // TODO set appearance "Keyboard": 0x03C1
+    // TODO declare primary service HID
+    let le_advertisement = Advertisement {
+        advertisement_type: bluer::adv::Type::Peripheral,
+        service_uuids: vec!["123e4567-e89b-12d3-a456-426614174000".parse().unwrap()]
+            .into_iter()
+            .collect(),
+        discoverable: Some(true),
+        // TODO take the name from a command line argument.
+        local_name: Some("Luc's bleyboard".to_string()),
+        ..Default::default()
+    };
+    println!("{:?}", &le_advertisement);
+    let handle = adapter.advertise(le_advertisement).await?;
+
+    println!("Press enter to quit");
+    let stdin = BufReader::new(tokio::io::stdin());
+    let mut lines = stdin.lines();
+    let _ = lines.next_line().await;
+
+    println!("Removing advertisement");
+    drop(handle);
+    sleep(Duration::from_secs(1)).await;
+
+    Ok(())
 }
