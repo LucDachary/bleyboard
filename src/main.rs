2c7ae314587680781ad7fb6575d8b465400927dc
LucDachary <LucDachary@users.noreply.github.com>
2022-10-31 09:50:28 +0100

    Adds ansi_term, cleans console output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ansi_term::Colour::{Green, Red};
 use bluer::{
     adv::Advertisement,
     adv::Type,
@@ -5,7 +6,7 @@ use bluer::{
         local::{
             characteristic_control, service_control, Application, Characteristic,
             CharacteristicControlEvent, CharacteristicNotify, CharacteristicNotifyMethod,
-            CharacteristicWrite, CharacteristicWriteMethod, Service,
+            CharacteristicRead, CharacteristicWrite, CharacteristicWriteMethod, Service,
         },
         CharacteristicReader, CharacteristicWriter,
     },
@@ -14,6 +15,7 @@ use bluer::{
 use futures::{future, pin_mut, StreamExt};
 use log::error;
 use log::LevelFilter;
+use log::{debug, info};
 use std::{collections::BTreeMap, time::Duration};
 use syslog::{BasicLogger, Facility, Formatter3164};
 use tokio::{
@@ -23,8 +25,12 @@ use tokio::{
 
 // Standard 128 bits UUID: 0000XXXX-0000-1000-8000-00805f9b34fb
 
-/// Human Interface Device (HID) service: 0x1812
-const SERVICE_UUID: uuid::Uuid = uuid::Uuid::from_u128(0x0000181200001000800000805f9b34fb);
+const SERVICE_BATTERY_UUID: uuid::Uuid = uuid::Uuid::from_u128(0x0000180f00001000800000805f9b34fb);
+const SERVICE_DEVICE_INFO_UUID: uuid::Uuid =
+    uuid::Uuid::from_u128(0x0000180a00001000800000805f9b34fb);
+const SERVICE_SCAN_PARAMS_UUID: uuid::Uuid =
+    uuid::Uuid::from_u128(0x0000181300001000800000805f9b34fb);
+const SERVICE_HID_UUID: uuid::Uuid = uuid::Uuid::from_u128(0x0000181200001000800000805f9b34fb);
 
 /// Characteristic UUID for GATT example.
 const CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xF00DC0DE00001);
@@ -34,10 +40,16 @@ const CHARACTERISTIC_UUID: uuid::Uuid = uuid::Uuid::from_u128(0xF00DC0DE00001);
 #[allow(dead_code)]
 const MANUFACTURER_ID: u16 = 0xf00d;
 /// Keyboard appearance.
-const APPEARANCE: u16 = 0x03c1;
+//const APPEARANCE_HID_KEYBOARD: u16 = 0x03c1;
+//const APPEARANCE_HID_MOUSE: u16 = 962;
+//const APPEARANCE_HID_JOYSTICK: u16 = 963;
+const APPEARANCE_HID_GAMEPAD: u16 = 964;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
+    println!("Starting Bleyboard…");
+
+    print!("Configuring syslog… ");
     let formatter = Formatter3164 {
         facility: Facility::LOG_USER,
         hostname: None,
@@ -45,41 +57,43 @@ async fn main() -> bluer::Result<()> {
         process: "bleyboard".into(),
         pid: 0,
     };
-
     let logger = syslog::unix(formatter).expect("could not connect to syslog");
     log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
         // LDA putting LevelFilter::Trace since bluez/bluer does have trace messages I need.
         .map(|()| log::set_max_level(LevelFilter::Trace))
         .unwrap();
+    println!("{}", Green.bold().paint("OK"));
 
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
     adapter.set_powered(true).await?;
 
-    println!(
+    print!("Configuring advertisement… ");
+    info!(
         "Advertising on Bluetooth adapter {} with address {}",
         adapter.name(),
         adapter.address().await?
     );
     let mut manufacturer_data = BTreeMap::new();
+    // DEV
     manufacturer_data.insert(MANUFACTURER_ID, vec![0x21, 0x22, 0x23, 0x24]);
-
-    // TODO declare primary service HID
     let le_advertisement = Advertisement {
         advertisement_type: Type::Peripheral,
-        service_uuids: vec![SERVICE_UUID].into_iter().collect(),
+        service_uuids: vec![SERVICE_BATTERY_UUID, SERVICE_HID_UUID]
+            .into_iter()
+            .collect(),
         manufacturer_data,
         discoverable: Some(true),
         // The keyboard appearance seems not to work when SERVICE_UUID is not the standard 0x1812.
-        appearance: Some(APPEARANCE),
+        appearance: Some(APPEARANCE_HID_GAMEPAD),
         // TODO use a commandline argument.
+        // Maximum is 180 seconds. See §5.1.1.
         timeout: Some(Duration::from_secs(30)),
         // TODO take the name from a command line argument.
         local_name: Some("Luc's bleyboard".to_string()),
         ..Default::default()
     };
-
-    println!("{:?}", &le_advertisement);
+    debug!("{:?}", &le_advertisement);
     let adv_handle = match adapter.advertise(le_advertisement).await {
         Ok(handle) => handle,
         Err(err) => match err.kind {
@@ -99,42 +113,122 @@ async fn main() -> bluer::Result<()> {
             }
         },
     };
+    println!("{}", Green.bold().paint("OK"));
 
-    println!(
+    debug!(
         "Serving GATT service on Bluetooth adapter {}",
         adapter.name()
     );
+
+    print!("Configuring HID Keyboard services\u{2026} ");
     let (service_control, service_handle) = service_control();
     let (char_control, char_handle) = characteristic_control();
-    let app = Application {
-        services: vec![Service {
-            uuid: SERVICE_UUID,
-            primary: true,
-            characteristics: vec![Characteristic {
-                uuid: CHARACTERISTIC_UUID,
-                write: Some(CharacteristicWrite {
-                    write: true,
-                    write_without_response: true,
-                    method: CharacteristicWriteMethod::Io,
-                    ..Default::default()
-                }),
-                notify: Some(CharacteristicNotify {
-                    notify: true,
-                    method: CharacteristicNotifyMethod::Io,
-                    ..Default::default()
-                }),
-                control_handle: char_handle,
+    let sbattery: Service = Service {
+        uuid: SERVICE_BATTERY_UUID,
+        primary: true,
+        characteristics: vec![Characteristic {
+            uuid: CHARACTERISTIC_UUID,
+            write: Some(CharacteristicWrite {
+                write: true,
+                write_without_response: true,
+                method: CharacteristicWriteMethod::Io,
                 ..Default::default()
-            }],
-            control_handle: service_handle,
+            }),
+            notify: Some(CharacteristicNotify {
+                notify: true,
+                method: CharacteristicNotifyMethod::Io,
+                ..Default::default()
+            }),
+            control_handle: char_handle,
             ..Default::default()
         }],
+        control_handle: service_handle,
+        ..Default::default()
+    };
+    let sdevice_info: Service = Service {
+        uuid: SERVICE_DEVICE_INFO_UUID,
+        primary: true,
+        characteristics: vec![
+            // TODO write the constant values in these characteristics.
+            Characteristic {
+                // Model Number 0x2A24
+                uuid: uuid::Uuid::from_u128(0x00002a2400001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Characteristic {
+                // Serial Number 0x2A25
+                uuid: uuid::Uuid::from_u128(0x00002a2500001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Characteristic {
+                // Firmware Revision 0x2A26
+                uuid: uuid::Uuid::from_u128(0x00002a2600001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Characteristic {
+                // Hardware Revision 0x2a27
+                uuid: uuid::Uuid::from_u128(0x00002a2700001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Characteristic {
+                // Software Revision 0x2a28
+                uuid: uuid::Uuid::from_u128(0x00002a2800001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Characteristic {
+                // Manufacturer 0x2a29
+                uuid: uuid::Uuid::from_u128(0x00002a2900001000800000805f9b34fb),
+                read: Some(CharacteristicRead {
+                    read: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+        ],
+        ..Default::default()
+    };
+    let sscan_params: Service = Service {
+        uuid: SERVICE_SCAN_PARAMS_UUID,
+        primary: false,
+        // TODO set characteristics
+        ..Default::default()
+    };
+    let shid: Service = Service {
+        uuid: SERVICE_HID_UUID,
+        primary: true,
+        // TODO set characteristics
+        ..Default::default()
+    };
+
+    let app = Application {
+        services: vec![sbattery, sdevice_info, sscan_params, shid],
         ..Default::default()
     };
     let app_handle = adapter.serve_gatt_application(app).await?;
+    println!("{}", Green.bold().paint("OK"));
 
-    println!("Service handle is 0x{:x}", service_control.handle()?);
-    println!("Characteristic handle is 0x{:x}", char_control.handle()?);
+    info!("Service handle is 0x{:x}", service_control.handle()?);
+    info!("Characteristic handle is 0x{:x}", char_control.handle()?);
 
     println!("Service ready. Press enter to quit.");
     let stdin = BufReader::new(tokio::io::stdin());
@@ -194,7 +288,7 @@ async fn main() -> bluer::Result<()> {
                         println!("Write request with {} bytes: {:x?}", n, &value);
                     }
                     Err(err) => {
-                        println!("Write stream error: {}", &err);
+                        println!("Write stream error: {}", Red.paint(format!("{}", &err)));
                         reader_opt = None;
                     }
                 }
